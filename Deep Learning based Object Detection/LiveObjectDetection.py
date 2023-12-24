39e676df482fc67999ecad3a225527acf4dc4b5f
ZachCraw <zachariahcraw@gmail.com>
2023-12-24 16:46:34 +1000

    First Commit

---
+++ Deep Learning based Object Detection/LiveObjectDetection.py
@@ -0,0 +1,120 @@
+# Import libraries
+import os
+import cv2
+import sys
+import numpy as np
+
+classFile  = "coco_class_labels.txt"
+
+with open(classFile) as fp:
+    labels = fp.read().split("\n")
+
+modelFile  = os.path.join("models", "ssd_mobilenet_v2_coco_2018_03_29", "frozen_inference_graph.pb")
+configFile = os.path.join("models", "ssd_mobilenet_v2_coco_2018_03_29.pbtxt")
+
+FONTFACE = cv2.FONT_HERSHEY_SIMPLEX
+FONT_SCALE = 0.7
+THICKNESS = 1
+
+# Read the Tensorflow network
+net = cv2.dnn.readNetFromTensorflow(modelFile, configFile)
+
+# For ach file in the directory
+def detect_objects(net, im, dim = 300):
+
+    # Create a blob from the image
+    blob = cv2.dnn.blobFromImage(im, 1.0, size=(dim, dim), mean=(0, 0, 0), swapRB=False, crop=False)
+
+    # Pass blob to the network
+    net.setInput(blob)
+
+    # Peform Prediction
+    objects = net.forward()
+    return objects
+
+
+def display_text(im, text, x, y):
+    # Get text size
+    textSize = cv2.getTextSize(text, FONTFACE, FONT_SCALE, THICKNESS)
+    dim = textSize[0]
+    baseline = textSize[1]
+
+    # Use text size to create a black rectangle
+    cv2.rectangle(
+        im,
+        (x, y - dim[1] - baseline),
+        (x + dim[0], y + baseline),
+        (0, 0, 0),
+        cv2.FILLED,
+    )
+
+    # Display text inside the rectangle
+    cv2.putText(
+        im,
+        text,
+        (x, y - 5),
+        FONTFACE,
+        FONT_SCALE,
+        (0, 255, 255),
+        THICKNESS,
+        cv2.LINE_AA,
+    )
+    
+
+def display_objects(im, objects, threshold=0.25):
+    rows = im.shape[0]
+    cols = im.shape[1]
+
+    # For every Detected Object
+    for i in range(objects.shape[2]):
+        # Find the class and confidence
+        classId = int(objects[0, 0, i, 1])
+        score = float(objects[0, 0, i, 2])
+
+        # Recover original cordinates from normalized coordinates
+        x = int(objects[0, 0, i, 3] * cols)
+        y = int(objects[0, 0, i, 4] * rows)
+        w = int(objects[0, 0, i, 5] * cols - x)
+        h = int(objects[0, 0, i, 6] * rows - y)
+
+        # Check if the detection is of good quality
+        if score > threshold:
+            display_text(im, "{}".format(labels[classId]), x, y)
+            cv2.rectangle(im, (x, y), (x + w, y + h), (255, 255, 255), 2)
+
+    cv2.imshow(win_name, im)
+
+s = 0
+if len(sys.argv) > 1:
+    s = sys.argv[1]
+
+source = cv2.VideoCapture(s)
+
+win_name = "AI Detection"
+cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+
+# Model parameters
+in_width = 300
+in_height = 300
+mean = (0, 0, 0)
+conf_threshold = 0.7
+
+while cv2.waitKey(1) != 27:
+    has_frame, frame = source.read()
+    if not has_frame:
+        break
+    frame = cv2.flip(frame, 1)
+    frame_height = frame.shape[0]
+    frame_width = frame.shape[1]
+
+    # Create a 4D blob from a frame.
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (in_width, in_height), mean, swapRB=False, crop=False)
+    # Run a model
+    net.setInput(blob)
+    detections = net.forward()
+
+    objects = detect_objects(net, frame)
+    display_objects(frame, objects)
+
+source.release()
+cv2.destroyWindow(win_name)
